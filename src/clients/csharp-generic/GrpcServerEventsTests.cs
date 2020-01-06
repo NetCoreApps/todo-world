014e23c559f055645a8d1a1f14b977ae675daa74
Demis Bellot <demis.bellot@gmail.com>
2020-01-06 09:00:40 -0500

    Update CSharpGeneric example with Server Events example

---
+++ src/clients/csharp-generic/GrpcServerEventsTests.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace CSharpGeneric
+{
+    public class GrpcServerEventsTests
+    {
+        [Test]
+        public async Task Can_stream_ServerEvents()
+        {
+            var client = Program.CreateClient();
+            var stream = client.StreamAsync(new StreamServerEvents {
+                Channels = new[] {"todos"}
+            });
+            
+            var todoTask = Program.Run(client);
+
+            var i = 0;
+            await foreach (var msg in stream)
+            {
+                var obj = JSON.parse(msg.Json);
+                if (msg.Selector.StartsWith("todos"))
+                {
+                    if (obj is Dictionary<string, object> map)
+                    {
+                        var id = map["id"];
+                        var title = map["title"];
+                        $"EVENT {msg.Selector} [{msg.Channel}]: #{id} {title}".Print();
+                    }
+                    else
+                    {
+                        $"EVENT {msg.Selector} [{msg.Channel}]: {obj}".Print();
+                    }
+                }
+                else
+                {
+                    $"EVENT {msg.Selector} [{msg.Channel}]: #{msg.UserId} {msg.DisplayName}".Print();
+                }
+                
+                if (++i >= 6) 
+                    break;
+            }
+
+            await todoTask;
+        }
+    }
+}
--- src/clients/csharp-generic/Program.cs
+++ src/clients/csharp-generic/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using ServiceStack;
 using ServiceStack.Text;
@@ -7,21 +8,30 @@ namespace CSharpGeneric
 {
     public class Program
     {
+        public static GrpcServiceClient CreateClient() => new GrpcServiceClient("https://localhost:5001",
+            new X509Certificate2("../../../../certs/dev.crt"),
+            GrpcUtils.AllowSelfSignedCertificatesFrom("localhost"));
+
         public static async Task Main(string[] args)
         {
             var client = new GrpcServiceClient("https://localhost:5001");
+            await Run(client);
+        }
+
+        public static async Task Run(GrpcServiceClient client)
+        {
             await client.PostAsync(new ResetTodos());
 
             //POST /todos
-            var todo = (await client.PostAsync(new CreateTodo { Title = "ServiceStack" })).Result;
+            var todo = (await client.PostAsync(new CreateTodo {Title = "ServiceStack"})).Result;
             $"new todo Id: {todo.Id}, Title: {todo.Title}".Print();
-            
+
             //GET /todos
             var all = await client.GetAsync(new GetTodos());
             $"todos: {all.Results?.Count ?? 0}".Print();
 
             //GET /todos/1
-            todo = (await client.GetAsync(new GetTodo { Id = todo.Id })).Result;
+            todo = (await client.GetAsync(new GetTodo {Id = todo.Id})).Result;
             $"get todo Id: {todo.Id}, Title: {todo.Title}".Print();
 
             //GET /todos
@@ -29,14 +39,14 @@ namespace CSharpGeneric
             $"todos: {all.Results?.Count ?? 0}".Print();
 
             //PUT /todos/1
-            await client.PutAsync(new UpdateTodo { Id = todo.Id, Title = "gRPC" });
+            await client.PutAsync(new UpdateTodo {Id = todo.Id, Title = "gRPC"});
 
             //GET /todos/1
-            todo = (await client.GetAsync(new GetTodo { Id = todo.Id })).Result;
+            todo = (await client.GetAsync(new GetTodo {Id = todo.Id})).Result;
             $"updated todo Title: {todo.Title}".Print();
 
             //DELETE /todos/1
-            await client.DeleteAsync(new DeleteTodo { Id = todo.Id });
+            await client.DeleteAsync(new DeleteTodo {Id = todo.Id});
 
             //GET /todos
             all = await client.GetAsync(new GetTodos());
